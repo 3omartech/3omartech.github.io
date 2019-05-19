5491666f8483033d2b28fb7564aa80e4d2a6571f
3omartech <50530911+3omartech@users.noreply.github.com>
2019-05-19 23:10:38 +0300

    Added test tweak

---
+++ AYAH/data/var/mobile/Library/iWidgets/AYAH/Config.js
@@ -0,0 +1,29 @@
+// Configuration Clock //
+
+
+
+	var ampm = false;	  // change to true to display 12h clock
+
+
+
+// Configuration Weather //
+
+
+
+       var isCelsius = true    // change to false to display degrees in Farenheit
+
+
+
+// Configuration Language //
+
+     
+
+	var French = false;	 // change to true to display weather and date in French
+
+	var German = false;	 // change to true to display weather and date in German
+
+	var Spanish = false;	 // change to true to display weather and date in Spanish
+
+	var Dutch = false;	 // change to true to display date in Dutch
+
+	var Italian = false;	 // change to true to display date in Italian
