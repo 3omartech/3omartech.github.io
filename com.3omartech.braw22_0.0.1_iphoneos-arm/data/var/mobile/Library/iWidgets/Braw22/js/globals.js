a49af511f2784027a149740f034f6115d180ed15
Omar.M <3omartech@mail.ru>
2020-01-15 07:13:47 +0200

    added a test tweak

---
+++ com.3omartech.braw22_0.0.1_iphoneos-arm/data/var/mobile/Library/iWidgets/Braw22/js/globals.js
@@ -0,0 +1,39 @@
+var options = {
+
+  },
+  cycript = {},
+  injectedWeather = {},
+  injectedSystem = {},
+  injectedMusic = {},
+  mainTarget = localStorage.themeName,
+  appToOpen = null,
+  urlToOpen = null,
+  wallpaperSet = false,
+  weather = null,
+  percent = ['0', '40', '60', '100'],
+  albumArtAdded = false,
+  screenShowing;
+
+
+function removeBackgroundAnimation(){
+    var bgAnimation = document.getElementById('backgroundAnimation');
+    if(bgAnimation){
+        document.body.removeChild(bgAnimation);
+    }
+}
+
+/* Note some versions of iOS may not support this */
+function handleVisibilityChange() {
+  if (document.hidden) {
+    screenShowing = false;
+    removeBackgroundAnimation();
+  } else  {
+    /* 
+        Animation is added throught the tweak as it knows what animation
+        the user chose.
+    */
+    screenShowing = true;
+    startLoop();
+  }
+}
+document.addEventListener("visibilitychange", handleVisibilityChange, false);
