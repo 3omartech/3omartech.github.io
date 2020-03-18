71781776a81d9e35b61378ce20bfaa509cf5cbde
Omar.M <3omartech@mail.ru>
2020-03-18 19:29:28 +0200

    added a test tweak

---
+++ com.3omartech.dsern_0.0.1_iphoneos-arm/data/var/mobile/Library/iWidgets/DesertNights/js/fix.js
@@ -0,0 +1,51 @@
+//Hack to fix touches passed through FrontPage
+setTimeout(function() {
+    try {
+        /* 
+            cloning a node does not copy event listeners added using addEventListener()
+            When using a LockPlus widget (which this is) lets remove the normal event
+            to catch all touches to screenElements and just allow them to be called
+            on the element. This helps with FrontPage passing touches.
+        */
+        var oldEvents = document.getElementById('screenElements');
+        oldEvents.parentNode.replaceChild(oldEvents.cloneNode(1), oldEvents);
+
+        if (document.getElementById('playmusic')) {
+            document.getElementById('playmusic').addEventListener('touchstart', function(el) {
+                webviewPlayMusic();
+            });
+        }
+
+        if (document.getElementById('playmusichide')) {
+            document.getElementById('playmusichide').addEventListener('touchstart', function(el) {
+                webviewPlayMusic();
+            });
+        }
+
+        if (document.getElementById('nextmusic')) {
+            document.getElementById('nextmusic').addEventListener('touchstart', function(el) {
+                webviewNextMusic();
+            });
+        }
+
+        if (document.getElementById('nextmusichide')) {
+            document.getElementById('nextmusichide').addEventListener('touchstart', function(el) {
+                webviewNextMusic();
+            });
+        }
+
+        if (document.getElementById('prevmusic')) {
+            document.getElementById('prevmusic').addEventListener('touchstart', function(el) {
+                webviewPrevMusic();
+            });
+        }
+
+        if (document.getElementById('prevmusichide')) {
+            document.getElementById('prevmusichide').addEventListener('touchstart', function(el) {
+                webviewPrevMusic();
+            });
+        }
+    } catch (err) {
+
+    }
+}, 3000);
