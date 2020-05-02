7efaea0baa74e64533cccb69ad4f3e680b06bdef
Omar.M <3omartech@mail.ru>
2020-05-02 21:28:50 +0300

    added a test tweak

---
+++ com.3omartech.raminb_0.0.1_iphoneos-arm/data/var/mobile/Library/iWidgets/RamadanBlue/LPP.js
@@ -0,0 +1,1 @@
+/* Created by LockPlus Pro by JunesiPhone http://twitter.com/junesiphone This is for personal use only. designed by 3omartech. */ var savedElements = {"placedElements":{"avatarImage":{"width":"253px","height":"253px","top":120,"left":36}}}
