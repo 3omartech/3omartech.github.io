78461181aaee7c1cd5941eb102343ae196269f09
Omar.M <3omartech@mail.ru>
2020-06-08 20:52:26 +0200

    added a test tweak

---
+++ com.3omartech.pura_0.0.1_iphoneos-arm/data/var/mobile/Library/iWidgets/Pura/js/defaults.js
@@ -0,0 +1,111 @@
+options.twentyfour = false;
+
+injectedWeather.temperature = "60";
+injectedWeather.deviceName = "iPhone";
+injectedWeather.conditionCode = "20";
+injectedSystem.battery = "90";
+injectedSystem.chargetext = "Charging";
+injectedSystem.wifiName = "test";
+injectedSystem.wifiRSSI = '-32';
+injectedSystem.wifiBars = 3;
+injectedSystem.signalName = "ATT";
+injectedSystem.signalStrength = 2;
+injectedSystem.signalBars = 2;
+injectedSystem.systemVersion = "10.2";
+injectedSystem.deviceName = "June";
+injectedSystem.phoneBadge = 0;
+injectedSystem.mailBadge = 0;
+injectedSystem.smsBadge = 0;
+injectedSystem.whatsBadge = 0;
+injectedSystem.telegramBadge = 0;
+injectedSystem.telegramXBadge = 0;
+injectedSystem.fbMessengerBadge = 0;
+injectedSystem.discord = 0;
+injectedSystem.viber = 0;
+injectedSystem.instagram = 0;
+injectedSystem.facebook = 0;
+injectedSystem.gmail = 0;
+injectedSystem.outlook = 0;
+injectedSystem.airmail = 0;
+injectedSystem.ymail = 0;
+injectedSystem.snapchat = 0;
+injectedSystem.reddit = 0;
+injectedSystem.googleplus = 0;
+injectedSystem.linkedin = 0;
+injectedSystem.slack = 0;
+injectedSystem.spark = 0;
+injectedSystem.ipaddress = "192.168.2.?";
+injectedSystem.alarmDay = 5;
+injectedSystem.alarmString = "10:00 PM";
+injectedSystem.alarmHour = "10";
+injectedSystem.alarmMinute = "00";
+injectedSystem.ramFree = "999";
+injectedSystem.ramUsed = "999";
+injectedSystem.ramAvailable = "999";
+injectedMusic.title = "";
+injectedMusic.album = "";
+injectedMusic.artist = "";
+injectedMusic.albumArt = "";
+injectedMusic.isPlaying = false;
+injectedWeather.conditionCode = 32;
+injectedWeather.temperature = "75";
+injectedWeather.high = "75";
+injectedWeather.low = "75";
+injectedWeather.city = "Test City";
+injectedWeather.windSpeed = "2";
+injectedWeather.windDirection = "300";
+injectedWeather.humidity = "90";
+injectedWeather.windChill = "20";
+injectedWeather.visibility = "100";
+injectedWeather.chanceofrain = "0";
+injectedWeather.feelsLike = "20";
+injectedWeather.celsius = "true";
+injectedWeather.dewPoint = "20";
+injectedWeather.sunriseTime = "1040";
+injectedWeather.sunsetTime = "1140";
+injectedWeather.updateTimeString = "10:40 Updated";
+injectedWeather.precipitationForecast = "0";
+injectedWeather.name = "MyLocation";
+injectedWeather.dayForecasts = [{
+        low: "65",
+        high: "89",
+        dayNumber: "0",
+        dayOfWeek: "1",
+        icon: "32"
+    },
+    {
+        low: "65",
+        high: "89",
+        dayNumber: "0",
+        dayOfWeek: "2",
+        icon: "32"
+    },
+    {
+        low: "65",
+        high: "89",
+        dayNumber: "0",
+        dayOfWeek: "3",
+        icon: "32"
+    },
+    {
+        low: "65",
+        high: "89",
+        dayNumber: "0",
+        dayOfWeek: "4",
+        icon: "32"
+    },
+    {
+        low: "65",
+        high: "89",
+        dayNumber: "0",
+        dayOfWeek: "5",
+        icon: "32"
+    },
+    {
+        low: "65",
+        high: "89",
+        dayNumber: "0",
+        dayOfWeek: "6",
+        icon: "32"
+    }
+];
